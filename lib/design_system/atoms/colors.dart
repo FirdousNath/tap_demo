87ee1a96a6dc3d5642fb32b2a3f516c260617cec
Firdous Nath <firdous.nath@alpha-vector.com>
2025-03-01 11:45:26 +0530

    feature: project setup

---
+++ lib/design_system/atoms/colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+final activeColor = Color.fromRGBO(0, 0, 0, 1);
+final bgColor = Color.fromRGBO(229, 231, 235, 1);
+final disabledColor = Color.fromRGBO(153, 161, 175, 1);
+
+final searchColor = Color.fromRGBO(229, 231, 235, 1);
+final highlightColor = Color.fromRGBO(217, 119, 6, 0.16);
+
+final blueColor = Color.fromRGBO(20, 71, 230, 1);
+final blueChipColor = Color.fromRGBO(37, 99, 235, 0.12);
+final successColor = Color.fromRGBO(5, 150, 105, 1);
+final successChipColor = Color.fromRGBO(5, 150, 105, 0.08);
+final errorColor = Color.fromRGBO(180, 83, 9, 1);
+final errorIconColor = Color.fromRGBO(217, 119, 6, 1);
+final errorBgColor = Color.fromRGBO(217, 119, 6, 0.12);
+
+final consColor = Color.fromRGBO(100, 116, 139, 1);
+final prosColor = Color.fromRGBO(54, 65, 83, 1);
+final descriptionColor = Color.fromRGBO(106, 114, 130, 1);
---
+++ lib/design_system/atoms/radius.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:tap_demo/design_system/atoms/sizes.dart';
+
+const microBorderRadius = BorderRadius.all(Radius.circular(micro));
+const smallBorderRadius = BorderRadius.all(Radius.circular(small));
+const mediumBorderRadius = BorderRadius.all(Radius.circular(medium));
+const largeBorderRadius = BorderRadius.all(Radius.circular(large));
